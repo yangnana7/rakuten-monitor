91f3aadfb24ae5f0bd6666a4924db7ed5673b72c
yangnana7 <roymunou7@yahoo.co.jp>
2025-07-29 01:54:08 +0900

    fix: add outer rakuten __init__.py

--- rakuten/__init__.py
+++ rakuten/__init__.py
@@ -1,18 +1,3 @@
-"""楽天商品監視システムパッケージ."""
-
-from .rakuten_parser import *  # noqa: F403
-from .item_db import *  # noqa: F403
-from .discord_notifier import *  # noqa: F403
-
-__version__ = "1.0.0"
-__all__ = [
-    # rakuten_parser
-    "parse_item_info",  # noqa: F405
-    "reset_known_items",  # noqa: F405
-    
-    # item_db
-    "ItemDB",  # noqa: F405
-    
-    # discord_notifier
-    "send_notification",  # noqa: F405
-]
+"""Namespace shim so that outer 'rakuten' behaves as a namespace package."""
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
