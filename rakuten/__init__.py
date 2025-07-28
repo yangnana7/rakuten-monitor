adf93912855a2f77892cbe44f94b601c143ed417
yangnana7 <roymunou7@yahoo.co.jp>
2025-07-29 02:11:12 +0900

    fix: include inner rakuten folder in namespace path

--- rakuten/__init__.py
+++ rakuten/__init__.py
@@ -3,3 +3,10 @@
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
+
+# Inner folder (.../rakuten/rakuten) を検索パスへ追加
+import pathlib as _pl
+
+_inner = _pl.Path(__file__).resolve().parent / "rakuten"
+if _inner.is_dir() and str(_inner) not in __path__:
+    __path__.append(str(_inner))
