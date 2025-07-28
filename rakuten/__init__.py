e3f77a6f23db909280364220d386af635e9f977d
yangnana7 <roymunou7@yahoo.co.jp>
2025-07-29 02:06:35 +0900

    fix: add outer rakuten __init__.py (namespace shim)

--- rakuten/__init__.py
+++ rakuten/__init__.py
@@ -1,3 +1,5 @@
-"""Namespace shim so that outer 'rakuten' behaves as a namespace package."""
+﻿"""Namespace shim so that outer 'rakuten' behaves as a namespace package."""
+
 from pkgutil import extend_path
+
 __path__ = extend_path(__path__, __name__)
